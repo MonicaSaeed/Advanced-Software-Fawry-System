66fd801fb9d3c9bb96aee308330496aaa3d04f2f
Monica Saeed <monicasaeed12@gmail.com>
2022-12-04 23:17:17 +0200

    update sign up and add sign in function

--- Project code/src/system/FawryUser.java
+++ Project code/src/system/FawryUser.java
@@ -6,8 +6,8 @@ public class FawryUser {
 	private String userName;
 	private String password;
 	private String email;
-	private String userType;  //Check if the user is an admin or not (admin -> true, user->false)
-	Vector<FawryUser> fawryUsers = new Vector<>(); //to save all users' data
+	private boolean userType;  //Check if the user is an admin or not (admin -> true, user->false)
+	static Vector<FawryUser> fawryUsers = new Vector<>(); //to save all users' data
 	
 	public String getUserName() {
 		return userName;
@@ -21,7 +21,7 @@ public class FawryUser {
 	public Vector<FawryUser> getFawryUsers() {
 		return fawryUsers;
 	}
-	public String getUserType() {
+	public boolean getUserType() {
 		return userType;
 	}
 
@@ -34,32 +34,60 @@ public class FawryUser {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public void setUserType(String userType) {
+	public void setUserType(Boolean userType) {
 		this. userType = userType;
 	}
 	
-	public void signUp(String userName,String password, String email, String userType ){
+	//public String signUp (String userName,String password, String email, Boolean userType ){
+	public String signUp (String userName, String email){
+		String found="yes";
         for(int i=0;i<fawryUsers.size();i++){
             FawryUser FU = fawryUsers.get(i);
             String UN = FU.getUserName();
             String E = FU.getEmail();
-            if(userName == UN ){
-                System.out.println("Invalid  user name");
+            if(userName == UN || email == E){
+                //System.out.println("Invalid  user name or email");
+				found = "no";
+				break;
             }
-            else if (email == E ){
-                System.out.println("Invalid  email");
-            }
-            else{
-                FawryUser FUValid = new FawryUser() ;
-                FUValid.setEmail(email);
-                FUValid.setPassword(password);
-                FUValid.setUserName(userName);
-                FUValid.setUserType(userType);
-                fawryUsers.add(FUValid);
-                System.out.println("Singed up successfully");
-            }
-
-        }        
-
+        }  
+		/* 
+		FawryUser FUValid = new FawryUser();
+		FUValid.setEmail(email);
+		FUValid.setPassword(password);
+		FUValid.setUserName(userName);
+		FUValid.setUserType(userType);
+		fawryUsers.add(FUValid);
+		//System.out.println("Singed up successfully");*/
+		return found;
     }
+	
+	public String signIn (String userName ,String password){
+		String userTybeReturn="";
+		boolean b=true;
+
+
+		for(int i=0;i<fawryUsers.size();i++){
+            FawryUser FU = fawryUsers.get(i);
+            String UN = FU.getUserName();
+            String p = FU.getPassword();
+            if(userName == UN && password==p ){
+				b=FU.getUserType();
+				userTybeReturn="yes";
+				break;
+            }
+        }
+
+		if(userTybeReturn=="yes"){
+			if(b==true){
+				return "admin";
+			}else{
+				return "user";
+			}
+		} //System.out.println("Singed in successfully");
+		else{
+			return "not";
+		} //System.out.println("invalid");
+	}
 }
+
